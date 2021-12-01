7ca40f828d31e282eca72afa785c8bdc0acd1b2a
NickRios <nickos.souza@hotmail.com>
2021-11-30 23:08:24 -0300

    🎉 initial commit

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,24 +3,37 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { ColorSchemeName, Pressable } from "react-native";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import TabOneScreen from "../screens/TabOneScreen";
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from "../types";
+import LinkingConfiguration from "./LinkingConfiguration";
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
       <RootNavigator />
     </NavigationContainer>
   );
@@ -35,7 +48,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
@@ -54,19 +71,21 @@ function BottomTabNavigator() {
       initialRouteName="TabOne"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
+      }}
+    >
       <BottomTab.Screen
         name="TabOne"
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Expressões Matematicas - Thiago Souza',
+        options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+          title: "Expressões Matematicas - Thiago Souza",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
-              onPress={() => navigation.navigate('Modal')}
+              onPress={() => navigation.navigate("Modal")}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
+              })}
+            >
               <FontAwesome
                 name="info-circle"
                 size={25}
@@ -85,7 +104,7 @@ function BottomTabNavigator() {
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,94 +1,92 @@
-import * as React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import * as React from "react";
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View, Input, TouchableButton } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View, Input, TouchableButton } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-import * as math from 'mathjs'
+import * as math from "mathjs";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const [expression, setExpression] = React.useState<string>("55 + 55");
+  const [result, setResult] = React.useState<number>(0);
 
-  const [expression,setExpression] = React.useState<string>("55 + 55");
-  const [result,setResult] = React.useState<number>(0);
-  
+  function evaluateResult(expressao: string) {
+    if (expressao == "") setResult(0);
 
-  function evaluateResult ( expressao : string){
-    if(expressao == "")  setResult(0); 
-   
     setResult(math.evaluate(expressao as math.MathExpression));
-  } 
+  }
 
-  function handlePressButton(){
+  function handlePressButton() {
     evaluateResult(expression);
   }
 
-
   return (
     <View style={styles.container}>
-      <Input style={styles.border} onChangeText={text => setExpression(text)}><Text>{expression}</Text></Input>
-      <Text style={styles.resultText} >{result}</Text>
+      <Input style={styles.border} onChangeText={(text) => setExpression(text)}>
+        <Text>{expression}</Text>
+      </Input>
+      <Text style={styles.resultText}>{result}</Text>
       <TouchableButton style={styles.button} onPress={handlePressButton}>
         <Text style={styles.textButton}>Calcular</Text>
       </TouchableButton>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
 
-
-
-
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
-  button:{
-    borderColor:'#FFF',
-    borderWidth:1,
-    width:'40%',
-    height:'20%',
-    borderRadius:5,
-    alignItems:'center',
-    justifyContent:'center'
+  button: {
+    borderColor: "#FFF",
+    borderWidth: 1,
+    width: "40%",
+    height: "20%",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  textField:{
-    borderWidth:1,
-    width:'20%',
-    borderRadius:4,
-    backgroundColor:'#FFFFFF',
-    fontSize:20,
-  },
-
-  textButton:{
-    fontSize:30,
-    
-  },
-  
-  resultText:{
-    fontSize:30,
-    margin:10,
+  textField: {
+    borderWidth: 1,
+    width: "20%",
+    borderRadius: 4,
+    backgroundColor: "#FFFFFF",
+    fontSize: 20,
   },
 
-  border:{
-    borderColor:'#FFFFFF',
-    borderWidth:4,
-    borderRadius:4,
-    padding:5,
-    width:'50%',
-  }
+  textButton: {
+    fontSize: 30,
+  },
 
+  resultText: {
+    fontSize: 30,
+    margin: 10,
+  },
+
+  border: {
+    borderColor: "#FFFFFF",
+    borderWidth: 4,
+    borderRadius: 4,
+    padding: 5,
+    width: "50%",
+  },
 });
